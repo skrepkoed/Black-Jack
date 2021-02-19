aed1f39d64e1c83933030c3e1181cc038397c52e
skrepkoed <faust898@mail.ru>
2021-02-19 22:37:27 +0500

    Добавлены методы для определения победителя и методы для поддержания цикла игры.

--- black_jack.rb
+++ black_jack.rb
@@ -10,6 +10,10 @@ class BlackJack
   def self.new_game
     puts 'Enter your name:'
     current_player = CurrnetPlayer.new(gets.chomp)
+    set_new_game
+  end
+
+  def self.set_new_game
     new(current_player, Diller.new, Bank.new, CardDeck.new).game
   end
 
@@ -23,10 +27,14 @@ class BlackJack
   end
 
   def game
-    card_deck.distribute_cards(2, current_player.hand)
-    card_deck.distribute_cards(2, diller.hand)
-    current_hand
-    send(*diller.next_move)
+    loop do
+      card_deck.distribute_cards(2, current_player.hand)
+      card_deck.distribute_cards(2, diller.hand)
+      current_hand
+      send(*diller.next_move)
+      current_hand
+      again?
+    end
   end
 
   def current_hand
@@ -41,6 +49,7 @@ class BlackJack
     game_options = { 'Draw additional card' => [:draw_card, current_player],
                      'Pass' => :pass,
                      'Face up' => :face_up }
+    game_options.delete 'Draw additional card' if current_player.hand.size == 3
     options = game_options.keys
     (1..options.size).each { |number| puts "#{number}. #{options[number - 1]}" }
     input = gets.chomp.to_i
@@ -49,7 +58,8 @@ class BlackJack
 
   def draw_card(player)
     card_deck.distribute_cards(1, player.hand)
-    evaluate_hand(player.hand)
+    puts "Current score: #{evaluate_hand(current_player.hand)}"
+    face_up if current_player.hand.size == 3
   end
 
   def pass; end
@@ -63,16 +73,35 @@ class BlackJack
   def define_winner
     current_player_score = evaluate_hand(current_player.hand)
     diller_score = evaluate_hand(diller.hand)
-    exceed_21?([current_player_score, diller_score])
-    case current_player <=> diller_score
-    when 1 then bank.give_gain(current_player)
-    when 0 then bank.draw(current_player, diller)
-    when -1 then bank.give_gain(diller)
+    if draw?(current_player, diller_score)
+      bank.draw(current_player, diller)
+      puts "Draw. Your current account: #{current_player.account}"
+    else
+      calculate_score(current_player, diller_score)
     end
   end
 
-  def exceed_21?(*hands_points)
-    hands_points.each { |hand| hand > 21 ? (raise ExceedPoints) : false }
+  def calculate_score(player1_score, player2_score)
+    case player1_score <=> player2_score
+    when 1
+      bank.give_gain(current_player)
+      puts "You win! Your current account: #{current_player.account}"
+    when -1
+      bank.give_gain(diller)
+      puts "You lose! Your current account: #{current_player.account}"
+    end
+  end
+
+  def draw?(player1_score, player2_score)
+    if player1_score == player2_score || exceed_21?(player1_score) && exceed_21?(player2_score)
+      true
+    else
+      false
+    end
+  end
+
+  def exceed_21?(hands_points)
+    hands_points > 21
   end
 
   def evaluate_hand(hand)
@@ -84,5 +113,15 @@ class BlackJack
 
   def define_card_point(card_rank)
     CARD_POINTS[card_rank] || card_rank
+  end
+
+  def again?
+    puts 'Would you like to try again? Y/N'
+    input = gets.chomp
+    if input == 'Y'
+      BlackJack.set_new_game
+    else
+      break
+    end
   end
 end
